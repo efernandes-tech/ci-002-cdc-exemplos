a940f906e74ad85daddbdf2a00efecaa8d8172fd
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 16:17:52 -0200

    Update

--- site-institucional/application/config/database.php
+++ site-institucional/application/config/database.php
@@ -76,28 +76,6 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ci-002-cdc-short-urls',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-$db['production'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
 	'username' => '',
 	'password' => '',
 	'database' => '',
