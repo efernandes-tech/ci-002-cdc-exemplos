fcc98eb73e995b98bfa5ad8d60fb887a58e984c1
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 09:43:27 -0200

    5.4 criando e configurando as rotas

--- site-institucional/application/config/routes.php
+++ site-institucional/application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Institucional';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['empresa'] = "Institucional/Empresa";
+$route['servicos'] = "Institucional/Servicos";
