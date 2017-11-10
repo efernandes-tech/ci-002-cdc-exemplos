c25b67e20ab9d5951c900b640d0f13c5a90a69b9
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 12:11:56 -0200

    6.1 configurando o cache para as páginas do site

--- site-institucional/application/controllers/Institucional.php
+++ site-institucional/application/controllers/Institucional.php
@@ -7,11 +7,15 @@ class Institucional extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        //Do your magic here
+
+        // Cache para todos os métodos dessa classe com uma saída de view.
+        $this->output->cache(1440);
     }
 
     public function index()
     {
+        $this->output->cache(1440); // 1440 corresponde a 24 horas até o cache ser atualizado.
+
         $data['title'] = "LCI | Home";
         $data['description'] = "Exercício de exemplo do CodeIgniter";
 
