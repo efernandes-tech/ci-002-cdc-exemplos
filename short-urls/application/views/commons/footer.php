980e882258671b766234c5cd918363438ea0cb4f
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 17:14:26 -0200

    13.2 criando as views

---
+++ short-urls/application/views/commons/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">Exemplo do capítulo 12 do livro <strong> CodeIgniter Teoria na Prática</strong></p>
+    </div>
+</footer>
+</body>
+</html>
