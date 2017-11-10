3c46606756841353326f894951b367e7ece79a23
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 15:27:42 -0200

    Update

--- site-institucional/application/views/commons/footer.php
+++ site-institucional/application/views/commons/footer.php
@@ -1,6 +1,6 @@
     <footer class="footer">
         <div class="container">
-            <p class="text-muted">&copy; Copyright 2016</p>
+            <p class="text-muted">&copy; Copyright <?php echo date('Y') ?></p>
         </div>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
