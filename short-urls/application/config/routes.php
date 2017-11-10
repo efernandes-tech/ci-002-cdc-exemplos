51c3e62e650863f38de413f9222422fa605b3463
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-11-10 16:20:20 -0200

    12.3 preparando as rotas

--- short-urls/application/config/routes.php
+++ short-urls/application/config/routes.php
@@ -49,6 +49,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Urls';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['login'] = "User/Login";
+$route['logout'] = "User/Logout";
+$route['user'] = "User/Index";
+$route['user/register'] = "User/Register";
+$route['minhas-urls'] = "User/Urls";
+$route['alterar-senha'] = "User/UpdatePassw";
+$route['(:any)'] = "Urls/Go";
