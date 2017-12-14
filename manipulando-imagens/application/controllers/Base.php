f18f529b4b0907a89013862d6c4c5cb694071f6e
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-14 17:27:48 -0200

    17.5 criando um thumbnail da imagem original

--- manipulando-imagens/application/controllers/Base.php
+++ manipulando-imagens/application/controllers/Base.php
@@ -1,10 +1,64 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Base extends CI_Controller {
+defined('BASEPATH') or exit('No direct script access allowed');
 
-	public function index()
-	{
-		$this->load->view('home');
-	}
+class Base extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('home');
+    }
+
+    public function Upload()
+    {
+        if (!$this->upload->do_upload('image')) {
+            $data['info'] = $this->upload->display_errors();
+        } else {
+            $data['info']        = "Imagem enviada com sucesso!";
+            $data['info_upload'] = $this->upload->data();
+
+            if($this->input->post('thumbnail')){
+                $configThumbnail['source_image'] = $data['info_upload']['full_path'];
+                $configThumbnail['maintain_ratio'] = TRUE;
+                $configThumbnail['width'] = 75;
+                $configThumbnail['height'] = 50;
+
+                $thumbnail = $this->GenThumbnail($configThumbnail);
+
+                if(!$thumbnail['status']){
+                    $data['info'] .= "<br/>Não foi possível gerar o thumbnail devido ao(s) erro(s) abaixo:<br />";
+                    $data['info'] .= $thumbnail['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = $data['info_upload']['file_path']
+                        ."/thumbs/".$data['info_upload']['raw_name']
+                        ."_thumb".$data['info_upload']['file_ext'];
+                }
+            }
+        }
+
+        $this->load->view('home', $data);
+    }
+
+    private function GenThumbnail($config)
+    {
+        $config['image_library'] = 'gd2';
+        $config['create_thumb'] = TRUE;
+        $config['new_image'] = "./uploads/thumbs/";
+
+        $this->image_lib->initialize($config);
+
+        if (!$this->image_lib->resize()){
+            $data['message'] = $this->image_lib->display_errors();
+            $data['status'] = false;
+        }else{
+            $data['message'] = null;
+            $data['status'] = true;
+        }
+
+        $this->image_lib->clear();
+
+        return $data;
+    }
+
 }
--- manipulando-imagens/application/views/home.php
+++ manipulando-imagens/application/views/home.php
@@ -1,15 +1,22 @@
 <?php $this->load->view('commons/header'); ?>
 
+<?php if(isset($info)) print_r($info);?>
+
 <div class="container">
   <div class="page-header">
     <h1>Manipulação de Imagens</h1>
   </div>
   <div class="row">
     <div class="col-md-12">
-      <form action="" method="POST" enctype="multipart/form-data">
+      <form action="<?=base_url('base/upload')?>" method="POST" enctype="multipart/form-data">
         <div class="form-group">
           <label>Selecione uma imagem</label>
           <input type="file" name="image" />
+        </div>
+        <div class="checkbox">
+          <label>
+          <input type="checkbox" name="thumbnail"> Criar thumbnail
+          </label>
         </div>
         <div class="form-group">
           <input type="submit" class="btn btn-success pull-right" value="Processar" />
