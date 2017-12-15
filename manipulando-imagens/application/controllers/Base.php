e3428ee3829c37b2623c11113ea92732fe429cb2
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-12-15 15:29:12 -0200

    capitulo 17 manipulando imagens

--- manipulando-imagens/application/controllers/Base.php
+++ manipulando-imagens/application/controllers/Base.php
@@ -35,6 +35,66 @@ class Base extends CI_Controller
                         ."_thumb".$data['info_upload']['file_ext'];
                 }
             }
+
+            if($this->input->post('width') || $this->input->post('height')){
+                $configResize['source_image'] = $data['info_upload']['full_path'];
+                $configResize['maintain_ratio'] = ($this->input->post('ratio')) ? TRUE : FALSE;
+                $configResize['width'] = ($this->input->post('width')) ? $this->input->post('width') : null;
+                $configResize['height'] = ($this->input->post('height')) ? $this->input->post('height') : null;
+
+                $resize = $this->ResizeImage($configResize);
+
+                if(!$resize['status']){
+                    $data['info'] .= "<br/>Não foi possível redimensionar a imagem devido ao(s) erro(s) abaixo:<br />";
+                    $data['info'] .= $resize['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = $data['info_upload']['file_path']
+                        ."/resized/".$data['info_upload']['raw_name']
+                        .$data['info_upload']['file_ext'];
+                }
+            }
+
+            if($this->input->post('rotation')){
+                $configRotate['source_image'] = $data['info_upload']['full_path'];
+                $configRotate['rotation_angle'] = $this->input->post('rotation');
+
+                $rotate = $this->RotateImage($configRotate);
+
+                if(!$rotate['status']){
+                    $data['info'] .= "<br/>Não foi possível redimensionar a imagem devido ao(s) erro(s) abaixo:<br />";
+                    $data['info'] .= $rotate['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = $data['info_upload']['file_path']
+                        ."/rotated/".$data['info_upload']['raw_name']
+                        .$data['info_upload']['file_ext'];
+                }
+            }
+
+            if($this->input->post('crop')){
+                $configCrop['source_image'] = $data['info_upload']['full_path'];
+                $crop = $this->CropImage($configCrop);
+                if(!$crop['status']){
+                    $data['info'] .= "<br/>Não foi possível recortar a imagem devido ao(s) erro(s) abaixo:<br />";
+                    $data['info'] .= $crop['message'];
+                }else{
+                    $data['info_upload']['thumb_path'] = $data['info_upload']['file_path']
+                        ."/cropped/".$data['info_upload']['raw_name']
+                        .$data['info_upload']['file_ext'];
+                }
+            }
+
+            if($this->input->post('watermark')){
+                $configWM['source_image'] = $data['info_upload']['full_path'];
+                $wm = $this->ApplyWatermark($configWM);
+                if(!$wm['status']){
+                    $data['info'] .= "<br/>Não foi possível aplicar a marca d'água na imagem devido ao(s) erro(s) abaixo:<br />";
+                    $data['info'] .= $wm['message'];
+                }else{
+                    $data['info_upload']['wm_path'] = $data['info_upload']['file_path']
+                        ."/watermark/".$data['info_upload']['raw_name']
+                        .$data['info_upload']['file_ext'];
+                }
+            }
         }
 
         $this->load->view('home', $data);
@@ -80,4 +140,66 @@ class Base extends CI_Controller
         return $data;
     }
 
+    private function RotateImage($config){
+        $config['image_library'] = 'gd2';
+        $config['new_image'] = "./uploads/rotated/";
+
+        $this->image_lib->initialize($config);
+
+        if (!$this->image_lib->rotate()){
+            $data['message'] = $this->image_lib->display_errors();
+            $data['status'] = false;
+        }else{
+            $data['message'] = null;
+            $data['status'] = true;
+        }
+
+        $this->image_lib->clear();
+
+        return $data;
+    }
+
+    private function CropImage($config){
+        $config['image_library'] = 'gd2';
+        $config['new_image'] = "./uploads/cropped/";
+        $config['width'] = 300;
+        $config['height'] = 300;
+        $config['x_axis'] = 50;
+        $config['y_axis'] = 50;
+
+        $this->image_lib->initialize($config);
+
+        if (!$this->image_lib->crop()){
+            $data['message'] = $this->image_lib->display_errors();
+            $data['status'] = false;
+        }else{
+            $data['message'] = null;
+            $data['status'] = true;
+        }
+
+        $this->image_lib->clear();
+
+        return $data;
+    }
+
+    private function ApplyWatermark($config){
+        $config['new_image'] = "./uploads/watermark/";
+        $config['wm_type'] = 'overlay';
+        $config['wm_overlay_path'] = './assets/images/watermark.png';
+        $config['wm_opacity'] = '50';
+
+        $this->image_lib->initialize($config);
+
+        if (!$this->image_lib->watermark()){
+            $data['message'] = $this->image_lib->display_errors();
+            $data['status'] = false;
+        }else{
+            $data['message'] = null;
+            $data['status'] = true;
+        }
+
+        $this->image_lib->clear();
+
+        return $data;
+    }
 }
--- manipulando-imagens/application/views/home.php
+++ manipulando-imagens/application/views/home.php
@@ -1,29 +1,70 @@
 <?php $this->load->view('commons/header'); ?>
 
-<?php if(isset($info)) print_r($info);?>
-
 <div class="container">
-  <div class="page-header">
-    <h1>Manipulação de Imagens</h1>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <form action="<?=base_url('base/upload')?>" method="POST" enctype="multipart/form-data">
-        <div class="form-group">
-          <label>Selecione uma imagem</label>
-          <input type="file" name="image" />
-        </div>
-        <div class="checkbox">
-          <label>
-          <input type="checkbox" name="thumbnail"> Criar thumbnail
-          </label>
-        </div>
-        <div class="form-group">
-          <input type="submit" class="btn btn-success pull-right" value="Processar" />
-        </div>
-      </form>
+    <div class="page-header">
+        <h1>Manipulação de Imagens</h1>
     </div>
-  </div>
+    <div class="row">
+        <div class="col-md-12">
+            <?php if(isset($info)): ?>
+                <div class="alert alert-info" role="alert">
+                    <?php echo $info; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <form action="<?=base_url('base/upload')?>" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label>Selecione uma imagem</label>
+                    <input type="file" name="image" />
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="thumbnail"> Criar thumbnail
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>Largura da Imagem após redimensionar (em pixels)</label>
+                    <input type="number" name="width" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label>Altura da Imagem após redimensionar (em pixels)</label>
+                    <input type="number" name="height" class="form-control" />
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="ratio"> Manter proporção
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>Girar Imagem?</label>
+                    <select name="rotation" class="form-control">
+                        <option value="">Não girar</option>
+                        <option value="90">90 graus</option>
+                        <option value="180">180 graus</option>
+                        <option value="270">270 graus</option>
+                        <option value="hor">Na Horizontal</option>
+                        <option value="vrt">Na vertical</option>
+                    </select>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="crop"> Recortar imagem?
+                    </label>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="watermark"> Inserir marca d'água
+                    </label>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-success pull-right" value="Processar" />
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 <?php $this->load->view('commons/footer'); ?>
