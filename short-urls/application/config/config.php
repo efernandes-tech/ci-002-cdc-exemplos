07366fb6b4747aa96d0757c79bc31ad2cbeb4a8f
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-11-29 23:52:45 -0200

    Update config.php

--- short-urls/application/config/config.php
+++ short-urls/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci-002-cdc-exemplos/short-urls/';
+// $config['base_url'] = 'http://ci-002-cdc-exemplos/short-urls/';
+$config['base_url'] = 'http://ci-002-cdc-exemplos.herokuapp.com/short-urls/';
 
 /*
 |--------------------------------------------------------------------------
