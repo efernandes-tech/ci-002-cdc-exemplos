fb09cb03eca1dbb2deb58d338971dc75e3ea46b3
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-11-29 23:35:46 -0200

    Update config.php

--- site-institucional/application/config/config.php
+++ site-institucional/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci-002-cdc-exemplos/site-institucional/';
+// $config['base_url'] = 'http://ci-002-cdc-exemplos/site-institucional/';
+$config['base_url'] = 'http://ci-002-cdc-exemplos.herokuapp.com/site-institucional/';
 
 /*
 |--------------------------------------------------------------------------
